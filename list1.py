74ace7a7f0d0df7d97a9867f5e1af86e75c21f9c
DavidRinSE <davidrichardsonin@gmail.com>
2020-01-30 14:17:13 -0500

    finished assignment

--- list1.py
+++ list1.py
@@ -23,10 +23,13 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 
+#        count += 1 if len(word) >= 2 and word[:2] == word[-2:] else 0
 
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    count = 0
+    for word in words:
+        count += 1 if len(word) >= 2 and word[0] == word[-1:] else 0
+    return count
 
 
 # B. front_x
@@ -36,9 +39,13 @@ def match_ends(words):
 # ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
+
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    xFiltered = list(filter(lambda x: x[0] == "x", words))
+    words = list(filter(lambda x: x[0] != "x", words))
+    xFiltered.sort()
+    words.sort()
+    return xFiltered + words
 
 
 # C. sort_last
@@ -48,8 +55,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
+    tuples.sort(key=lambda x: x[-1:])
+    return tuples
 
 
 # Simple provided test() function used in main() to print
--- list2.py
+++ list2.py
@@ -18,8 +18,11 @@
 
 
 def remove_adjacent(nums):
-    """Your code goes here.  Edit this docstring."""
-    return
+    output = []
+    for num in nums:
+        if not output or output[-1] != num:
+            output.append(num)
+    return output
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -27,8 +30,14 @@ def remove_adjacent(nums):
 # The solution should work in "linear" time, making a single pass of both lists.
 # Hint: Don't use `sort` or `sorted` -- they are not linear time.
 def linear_merge(list1, list2):
-    """Your code goes here.  Edit this docstring."""
-    return
+    newList = []
+    while list1 and list2:
+        if list1[0] < list2[0]:
+            newList.append(list1.pop(0))
+        else:
+            newList.append(list2.pop(0))
+    newList += list1 if not list2 else list2
+    return newList
 
 
 # Simple provided test() function used in main() to print
